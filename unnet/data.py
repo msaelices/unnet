e293cb13001e1a7b1e3c5e58b5c04b3079ddb215
Manuel Saelices <msaelices@gmail.com>
2022-12-11 00:35:48 +0100

    Track parents

--- unnet/data.py
+++ unnet/data.py
@@ -1,25 +1,41 @@
 from __future__ import annotations
 
+from enum import Enum
+
+ADD = '+'
+SUB = '-'
+MUL = '*'
+
 
 class Node:
     """Representation of an expression node capable of performing math operations and calculate the back propagation"""
 
-    def __init__(self, value: int | float) -> None:
+    def __init__(
+        self,
+        value: int | float,
+        op: str | None = None,
+        parents: tuple | None = None,
+    ) -> None:
         self.value = value
+        self.parents = parents or ()
+        self.op = op
 
     def __repr__(self) -> str:
         return f"Node({self.value})"
+    
+    def __hash__(self) -> int:
+        return id(self)
 
     def __add__(self, other) -> Node:
-        return Node(self.value + other.value)
+        return Node(self.value + other.value, op=ADD, parents=(self, other))
 
     def __sub__(self, other) -> Node:
-        return Node(self.value - other.value)
+        return Node(self.value - other.value, op=SUB, parents=(self, other))
 
     def __mul__(self, other) -> Node:
-        return Node(self.value * other.value)
+        return Node(self.value * other.value, op=MUL, parents=(self, other))
 
     def __eq__(self, other) -> Node:
         return self.value == other.value
 
-    # TODO: Track parents and calculate back propagation
+    # TODO: Calculate back propagation
