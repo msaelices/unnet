548bb06fca58fe195e695cbd808a0a32cfb40cc6
Manuel Saelices <msaelices@gmail.com>
2022-12-11 23:53:41 +0100

    Implement forward propagation in neural networks

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -1,4 +1,4 @@
-from unnet.nn import Neuron
+from unnet.nn import Neuron, Layer, Network
 
 
 def test_rand_neuron():
@@ -12,3 +12,25 @@ def test_neuron_output():
     neuron = Neuron(weights=[0.5, 2], bias=0.2)
     result = neuron.out([2.0, 3.0])
     assert result.value == 7.2
+
+
+def test_layer_output():
+    neuron1 = Neuron(weights=[0.5, 0.8], bias=0.2)
+    neuron2 = Neuron(weights=[0.2, -0.2], bias=0.9)
+    layer = Layer([neuron1, neuron2])
+    results = layer.out([2.0, 3.0])
+    assert results[0].value == 3.6000000000000005
+    assert results[1].value == 0.7
+
+
+def test_network_output():
+    neuron1 = Neuron(weights=[0.5, 0.8], bias=0.2)
+    neuron2 = Neuron(weights=[0.2, -0.2], bias=0.9)
+    neuron3 = Neuron(weights=[-0.5, 0.3], bias=-0.2)
+    neuron4 = Neuron(weights=[-0.2, 0.2], bias=0.4)
+    layer1 = Layer([neuron1, neuron2])
+    layer2 = Layer([neuron3, neuron4])
+    network = Network([layer1, layer2])
+    results = network.out([2.0, 3.0])
+    assert results[0].value == -1.7900000000000003
+    assert results[1].value == -0.18000000000000016
--- unnet/nn.py
+++ unnet/nn.py
@@ -15,9 +15,12 @@ class Neuron:
         self.weights = [Node(w, name=f'w{i}') for i, w in enumerate(weights, 1)]
         self.bias = Node(bias, name='bias')
 
-    def out(self, inputs: list[float]):
+    def __repr__(self) -> str:
+        return f'Neuron(num_inputs={len(self.weights)})'
+
+    def out(self, inputs: list[float]) -> Node:
         # TODO: Implement an activation functions
-        input_nodes = [Node(x, name=f'x{i}') for i, x in enumerate(inputs)]
+        input_nodes = [x if isinstance(x, Node) else Node(x, name=f'x{i}') for i, x in enumerate(inputs)]
         # Separate the first input and weight so the sum function give us a closer plot when drawing the resulting node
         w1, *remaining_weights = self.weights
         x1, *remaining_inputs = input_nodes
@@ -27,3 +30,32 @@ class Neuron:
     def rand_neuron(cls, num_inputs: int, bias: float = 0.0):
         weights = [random.uniform(-1.0, 1.0) for _ in range(num_inputs)]
         return cls(weights=weights, bias=bias)
+
+
+@dataclass
+class Layer:
+    neurons: list[Neuron]
+
+    def __repr__(self):
+        return f'Layer(neurons={", ".join(str(n) for n in self.neurons)})'
+
+    def out(self, inputs: list[float]) -> list[Node]:
+        return [n.out(inputs) for n in self.neurons]
+
+    @classmethod
+    def rand_layer(cls, num_neurons: int, num_inputs: int):
+        neurons = [Neuron.rand_neuron(num_inputs) for _ in range(num_neurons)]
+        return cls(neurons)
+
+
+@dataclass
+class Network:
+    layers: list[Layer]
+
+    def __repr__(self):
+        return f'Network(num_layers={len(self.layers)})'
+
+    def out(self, inputs: list[float]) -> list[Node]:
+        for layer in self.layers:
+            inputs = layer.out(inputs)
+        return inputs
